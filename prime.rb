b66b99c1bfb776c0060c733dc72d940ac4503493
Alex Siler <davidalexsiler@gmail.com>
2019-05-22 21:42:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 def prime?(num)
-  a = Array 
-    #numbers that cannot be made by multiplying other whole numbers 
+  a = Array (2..num-1)
+  #make a range of numbers 2-number before numbers
+  #
+  #prime is numbers that cannot be made by multiplying other whole numbers 
 end
 
