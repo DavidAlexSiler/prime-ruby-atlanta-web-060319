c8f50c908816c6edbb7b0a6fb93c73810f734ea4
Alex Siler <davidalexsiler@gmail.com>
2019-05-22 21:55:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 def prime?(num)
-  if num <= 0
-    return false
-  else
   (2..(num -1)).each do |n|
-    return false if num % n == 0 
+    return false if num < 0 || num % n == 0 
   end
   true
 end
-end
+
