7a51b921c9ea8d49c4ff524422cd43e78fff8510
Alex Siler <davidalexsiler@gmail.com>
2019-05-22 21:44:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(num)
   (2..(num -1)).each do |n|
-    
+    return false if num % 
   #make a range of numbers 2-number before numbers
   #
   #prime is numbers that cannot be made by multiplying other whole numbers 
